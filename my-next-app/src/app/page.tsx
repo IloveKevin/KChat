2386a901543809c7d58e7a8af69aadf6c301436d
Kz12138 <804411280@qq.com>
2023-08-30 16:55:44 +0800

    添加my-nextjs-app

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='fixed w-full h-full bg-red-500'>
+      <div className='fixed flex items-center justify-center top-1/2 left-1/2 transform translate-x-[-50%] translate-y-[-50%] w-full md:w-64 h-64 bg-blue-500'>
+        <h1 className=''>这里是登录界面</h1>
+        </div>
     </div>
   )
 }
